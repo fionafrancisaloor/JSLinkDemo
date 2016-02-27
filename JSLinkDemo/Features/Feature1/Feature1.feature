a8568508f3fec5bf758a9a7a35b53f6a06c374a9
spprashaant <thesharepointguide@gmail.com>
2016-02-27 14:46:33 +0530

    v1.0

---
+++ JSLinkDemo/Features/Feature1/Feature1.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="540d44f0-4053-416c-9025-f004abcb49ad" description="SharePoint App Feature" featureId="540d44f0-4053-416c-9025-f004abcb49ad" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="JSLinkDemo Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="c4d9e9b9-4a8b-4681-8b04-18d18890c78e" />
+    <projectItemReference itemId="e20079f3-3a80-46de-b925-4e0e4030ad87" />
+    <projectItemReference itemId="ee9ba839-775b-42de-83a2-ea298be64dbf" />
+    <projectItemReference itemId="ef492bdf-74af-48eb-aee3-224bd03089a5" />
+    <projectItemReference itemId="305aa1e3-a649-458b-ac6f-b3e695ce4cdb" />
+    <projectItemReference itemId="883ac69f-8afa-454b-92d5-6df982eb8386" />
+  </projectItems>
+</feature>
---
+++ JSLinkDemo/pkg/Debug/JSLinkDemo/JSLinkDemo_Feature1/Scripts/NewFormScript.js
@@ -0,0 +1,37 @@
+﻿(function () {
+  var ctx = {};
+  
+  ctx.OnPostRender = updateDOM;
+
+  SPClientTemplates.TemplateManager.RegisterTemplateOverrides(ctx);
+})();
+
+function updateDOM(ctx) {
+  (function ($) {
+    $(document).ready(function () {
+      var current = 1;
+
+      //create a new table element which is same as current table element we want to replace
+      var newTable ='<table class="ms-formtable" style="margin-top: 8px;" border="0" cellpadding="0" cellspacing="0" width="100%"><tr>';
+
+      //loop through each td in the table
+      $(".ms-formtable td").each(function (i) {
+        //in the first row, I wanted only one column
+        // each field is a set of 2 columns one label and other input control
+        // if current column is 2, we want to start a new row similarly if it is 6(next 2 fields)
+        if (current == 2 || current == 6) {
+          newTable = newTable + $('<div>').append($(this).clone()).remove().html() + '</tr><tr>';
+        }
+        else {
+
+          newTable = newTable + $('<div>').append($(this).clone()).remove().html();
+        }
+        current = current + 1;
+      });
+      newTable = newTable + '</tr></table>';
+
+      // replace the html of existing table with the one we created.
+      $(".ms-formtable").html(newTable);
+    });
+  })(jQuery);
+}
